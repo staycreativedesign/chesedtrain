f50a2583c9da8d6abf58066af6c112c97d64e669
Gus P <gustavoanalytics@gmail.com>
2024-12-29 14:18:44 -0500

    foo

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,12 @@
 class ContactController < ApplicationController
-  def new
-  end
+  def new; end
 
   def create
+    ContactMailer.with(first_name: params[:first_name],
+                       last_name: params[:last_name],
+                       email_address: params[:email_address],
+                       message: params[:message]).contact_message.deliver_now
+
+    redirect_to contact_thank_you_path
   end
 end
