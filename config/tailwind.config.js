d50206553899c23b589ca1e34549b903384d576d
Gus P <gustavoanalytics@gmail.com>
2024-12-29 00:09:41 -0500

    foo

---
+++ config/tailwind.config.js
@@ -0,0 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}',
+    './node_modules/flowbite/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'primary': 'rgba(63,114,175,0.2)',
+        'primary-original': '#3F72AF',
+        'secondary': '#123D4E',
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+    require('flowbite/plugin')
+  ]
+}
+
