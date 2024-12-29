7ca6119e5f11332e243d8d5a956f86224d7df20f
Gus P <gustavoanalytics@gmail.com>
2024-12-29 15:08:54 -0500

    foo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
-      start_new_session_for user
+      session[:user_id] = user.id
       redirect_to after_authentication_url
     else
       redirect_to new_session_path, alert: 'Try another email address or password.'
