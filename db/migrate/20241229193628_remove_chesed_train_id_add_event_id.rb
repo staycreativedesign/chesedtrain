4da7e4d968149cede7084c74845e00b2ae7a7bce
Gus P <gustavoanalytics@gmail.com>
2024-12-29 14:40:45 -0500

    foo

--- db/migrate/20241229193628_remove_chesed_train_id_add_event_id.rb
+++ db/migrate/20241229193628_remove_chesed_train_id_add_event_id.rb
@@ -1,6 +1,5 @@
 class RemoveChesedTrainIdAddEventId < ActiveRecord::Migration[7.0]
   def change
-    remove_column :event_dates, :chesed_train, index: true, foreign_key: true
     add_reference :event_dates, :event, index: true, foreign_key: true
   end
 end
