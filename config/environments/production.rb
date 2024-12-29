59290d2299e8c8ae4e6c8a96bcc83117ebb57e45
Gus P <gustavoanalytics@gmail.com>
2024-12-29 15:39:35 -0500

    foo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,8 @@ Rails.application.configure do
     host: 'chesedtrain.com'
   }
 
+  config.action_mailer.default_url_options = { host: 'chesedtrain.com', protocol: 'https' }
+
   config.hosts = [
     'chesedtrain.com',     # Allow requests from example.com
     /.*\.chesedtrain\.com/ # Allow requests from subdomains like `www.example.com`
