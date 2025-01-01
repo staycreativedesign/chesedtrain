af74b51096c56cd2bc30707b68566b45d069df42
Gus P <gustavoanalytics@gmail.com>
2025-01-01 14:47:35 -0500

    foo

--- db/migrate/20241229192103_drop_chesed_trains_and_potlucks.rb
+++ db/migrate/20241229192103_drop_chesed_trains_and_potlucks.rb
@@ -1,6 +1,6 @@
 class DropChesedTrainsAndPotlucks < ActiveRecord::Migration[7.0]
   def change
-    drop_table :chesed_trains
-    drop_table :potlucks
+    drop_table :chesed_trains, force: :cascade
+    drop_table :potlucks, force: :cascade
   end
 end
--- db/migrate/20241229193628_remove_chesed_train_id_add_event_id.rb
+++
@@ -1,5 +0,0 @@
-class RemoveChesedTrainIdAddEventId < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :event_dates, :event, index: true, foreign_key: true
-  end
-end
