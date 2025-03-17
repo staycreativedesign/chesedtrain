7665a2a74b8b9d67d45f650644a316531390358c
Gus P <gustavoanalytics@gmail.com>
2025-03-17 13:02:58 -0400

    foo

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,11 +2,17 @@ class ContactController < ApplicationController
   def new; end
 
   def create
-    ContactMailer.with(first_name: params[:first_name],
-                       last_name: params[:last_name],
-                       email_address: params[:email_address],
-                       message: params[:message]).contact_message.deliver_now
+    if verify_recaptcha(message: 'Cannot verify your recaptcha')
 
-    redirect_to contact_thank_you_path
+      ContactMailer.with(first_name: params[:first_name],
+                         last_name: params[:last_name],
+                         email_address: params[:email_address],
+                         message: params[:message]).contact_message.deliver_now
+
+      redirect_to contact_thank_you_path
+    else
+      flash[:notice] = 'Email not found'
+      redirect_to new_contact_path
+    end
   end
 end
