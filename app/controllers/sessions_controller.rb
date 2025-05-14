a5a6ba57c064f2fa09e9292561d37d400b5bcdc9
Gus P <gustavoanalytics@gmail.com>
2025-05-14 11:22:00 -0400

    foo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,13 @@ class SessionsController < ApplicationController
     @event = params[:event]
     @event_id = params[:event_id]
     @selection = params[:selection_id]
+    @steps = params[:steps]
   end
 
   def create
     user = User.find_by(email_address: params[:email_address])
-    if user&.authenticate(params[:password]) && verify_recaptcha(message: 'Cannot verify your recaptcha')
+    if user&.authenticate(params[:password])
+      # if user&.authenticate(params[:password]) && verify_recaptcha(message: 'Cannot verify your recaptcha')
       session[:user_id] = user.id
       @current_user = user
       redirect_to determine_redirect_path(params)
@@ -24,13 +26,25 @@ class SessionsController < ApplicationController
   private
 
   def determine_redirect_path(params)
-    return user_path(@current_user) unless params[:event].present?
+    return user_path(@current_user) if params[:event].blank?
 
     case params[:event]
+    when 'potluck'
+      event = Event.find(params[:event_id])
+      if params[:steps].present? && event.owner.guest?
+        event.update(owner: @current_user)
+        potluck_path(event)
+      else
+        volunteer_potluck_selection_path(params[:event_id], params[:selection_id])
+      end
     when 'chesedtrain'
-      volunteer_chesed_train_event_date_path(params[:event_id], params[:selection_id])
-    else
-      volunteer_potluck_selection_path(params[:event_id], params[:selection_id])
+      event = Event.find(params[:event_id])
+      if params[:steps].present? && event.owner.guest?
+        event.update(owner: @current_user)
+        chesed_train_path(event)
+      else
+        volunteer_chesed_train_event_date_path(params[:event_id], params[:selection_id])
+      end
     end
   end
 end
